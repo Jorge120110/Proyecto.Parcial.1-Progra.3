1b65a5dd3d8f23927ea2dbc6d9a8b8bdbd511697
Jorge <jorgecobon43@gmail.com>
2026-03-12 20:23:27 -0600

    Primera carga de documentacion

---
+++ RabbitMQ-Parcial1/src/main/java/modelos/Transaccion.java
@@ -0,0 +1,81 @@
+package modelos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Transaccion {
+    private String idTransaccion;
+    private String nombre;
+    private String carnet;
+    private Double monto;
+    private String moneda;
+    private String cuentaOrigen;
+    private String bancoDestino;
+    private Detalle detalle;
+   
+
+
+    public String getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public void setIdTransaccion(String idTransaccion) {
+        this.idTransaccion = idTransaccion;
+    }
+
+    public Double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Double monto) {
+        this.monto = monto;
+    }
+
+    public String getMoneda() {
+        return moneda;
+    }
+
+    public void setMoneda(String moneda) {
+        this.moneda = moneda;
+    }
+
+    public String getCuentaOrigen() {
+        return cuentaOrigen;
+    }
+
+    public void setCuentaOrigen(String cuentaOrigen) {
+        this.cuentaOrigen = cuentaOrigen;
+    }
+
+    public String getBancoDestino() {
+        return bancoDestino;
+    }
+
+    public void setBancoDestino(String bancoDestino) {
+        this.bancoDestino = bancoDestino;
+    }
+
+    public Detalle getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(Detalle detalle) {
+        this.detalle = detalle;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+}
